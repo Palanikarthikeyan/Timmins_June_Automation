49c940f1c98d43b0377d174a78058953370eda27
Palanikarthikeyan <abpalanikarthik@gmail.com>
2026-04-14 22:30:07 +0530

    Create demo1.py

---
+++ DAY2/Advanced Data Processing/usecase1/demo1.py
@@ -0,0 +1,11 @@
+'''
+1) Wafer Test Data Analysis (CSV Processing + Filtering + Summary)
+Business Problem
+Semiconductor manufacturing generates wafer probe test results in CSV files.
+
+Engineers need to:
+read test data
+filter failed dies
+calculate yield %
+summarize per wafer lot
+'''
